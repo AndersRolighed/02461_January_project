0c325d92ec5678c1c78cdf858cba9ff85648e094
RasmusTorp <s214597@dtu.dk>
2022-01-05 13:48:09 +0100

    Update plot_function.py

--- plot_function.py
+++ plot_function.py
@@ -5,17 +5,11 @@ Created on Wed Jan  5 13:13:47 2022
 @author: Rasmus
 """
 import matplotlib.pyplot as plt
+import numpy as np
+import torch 
+#funktionen tager numpy 
 
-def plot_data():
-    
-    n_slice = 75
-    n_image = 0
-    
-    # Data location
-    test_image = np.load(pwd + 'BraTS2020/input_data_4channels/images/image_' + str(n_image) + '.npy')
-    test_seg   = np.load(pwd + 'BraTS2020/input_data_4channels/masks/mask_' + str(n_image) + '.npy')
-    
-    # PyPlot figure
+def plot_2d_data(test_image,test_mask,n_slice):
     plt.figure(figsize=(12, 8))
     
     plt.subplot(241)
@@ -31,16 +25,27 @@ def plot_data():
     plt.imshow(test_image[3,:,:,n_slice], cmap='gray')
     plt.title('Modality 4')
     plt.subplot(245)
-    plt.imshow(test_seg[0,:,:,n_slice], cmap='gray')
+    plt.imshow(test_mask[0,:,:,n_slice], cmap='gray')
     plt.title('Mask 1')
     plt.subplot(246)
-    plt.imshow(test_seg[1,:,:,n_slice], cmap='gray')
+    plt.imshow(test_mask[1,:,:,n_slice], cmap='gray')
     plt.title('Mask 2')
     plt.subplot(247)
-    plt.imshow(test_seg[2,:,:,n_slice], cmap='gray')
+    plt.imshow(test_mask[2,:,:,n_slice], cmap='gray')
     plt.title('Mask 3')
     plt.subplot(248)
-    plt.imshow(test_seg[3,:,:,n_slice], cmap='gray')
+    plt.imshow(test_mask[3,:,:,n_slice], cmap='gray')
     plt.title('Mask 4')
     
     plt.show()
+
+test_image = torch.from_numpy(np.load("C:/Users/Rasmus/Desktop/MRI-projekt/02461_January_project/BraTS2020/train_valid_data/train/images/image_0.npy"))
+
+test_mask  = torch.from_numpy(np.load("C:/Users/Rasmus/Desktop/MRI-projekt/02461_January_project/BraTS2020/train_valid_data/train/masks/mask_0.npy"))
+
+print(type(test_image))
+print(type(test_mask))
+plot_2d_data(test_image,test_mask,75)
+
+
+
