92919bed9550bce36d3fb9df30891b982b25d5ae
RasmusTorp <s214597@dtu.dk>
2022-01-13 12:52:25 +0100

    Nye eval funktioner

--- dice_score.py
+++ dice_score.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 12 09:30:24 2022
+Created on Thu Jan 13 12:16:28 2022
 
 @author: Rasmus
 """
 
-# fra - https://www.kaggle.com/iezepov/fast-iou-scoring-metric-in-pytorch-and-numpy
-
 import torch
 
-def iou_score(outputs: torch.Tensor, labels: torch.Tensor):
+def dice_score(outputs: torch.Tensor, labels: torch.Tensor):
     SMOOTH = 1e-6
     # You can comment out this line if you are passing tensors of equal shape
     # But if you are passing output from UNet or something it will most probably
@@ -19,8 +17,8 @@ def iou_score(outputs: torch.Tensor, labels: torch.Tensor):
     intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
     union = (outputs | labels).float().sum((1, 2))         # Will be zero if both are 0
     
-    iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+    dice = (2*intersection + SMOOTH) / (union + intersection + SMOOTH)  # We smooth our devision to avoid 0/0
     
-    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    thresholded = torch.clamp(20 * (dice - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
     
     return thresholded  # Or thresholded.mean() if you are interested in average across the batch
