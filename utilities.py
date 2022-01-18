277e31932ec7fad9c823b477871990ff9a875ae1
jesperdlau <jesperdlau@gmail.com>
2022-01-18 10:09:54 +0100

    added 2 plotfunctions

--- utilities.py
+++ utilities.py
@@ -121,7 +121,7 @@ def batchLoad_single(directory, fileindex):
 
 
 #%% 
-def plot_prediction_mask(prediction, mask):
+def plot_prediction_mask_max(prediction, mask):
     mask_np = mask.detach().numpy()
     max = 0
     for i in range(128):
@@ -154,3 +154,79 @@ def load_checkpoint(checkpoint, model, optimizer):
     print("=> Loading checkpoint")
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
+    
+    
+
+#%% 
+def plot_prediction_mask(prediction, mask, idx):
+    pred_argmax = np.argmax(prediction.detach().numpy(), axis=1)
+    mask_argmax = np.argmax(mask.detach().numpy(), axis=1)
+    
+    print(np.shape(pred_argmax), np.shape(mask_argmax))
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(pred_argmax[0,:,:,idx])
+    plt.title("Predicted mask")
+    plt.subplot(122)
+    plt.imshow(mask_argmax[0,:,:,idx])
+    plt.title("Original mask")
+    
+    # plt.suptitle("")
+    plt.show()
+
+#%%
+# Input: prediction as tensor, mask as tensor, image number as int, index as int, and extra info as string (fx. )
+def plot_combined(prediction, mask, img_n, idx, info):
+    prediction = prediction.detach().numpy()
+    mask = mask.detach().numpy()
+    pred_argmax = np.argmax(prediction, axis=1)
+    mask_argmax = np.argmax(mask, axis=1)
+    
+    plt.figure(figsize=(10, 4))
+    plt.suptitle(f"Image: {img_n}  -  Slice: {idx}  -  Info: {info}")
+    
+    ax1 = plt.subplot2grid((2,5), (0,0))
+    ax2 = plt.subplot2grid((2,5), (0,1))
+    ax3 = plt.subplot2grid((2,5), (0,2))
+    ax4 = plt.subplot2grid((2,5), (0,3))
+    ax5 = plt.subplot2grid((2,5), (0,4))
+    ax6 = plt.subplot2grid((2,5), (1,0))
+    ax7 = plt.subplot2grid((2,5), (1,1))
+    ax8 = plt.subplot2grid((2,5), (1,2))
+    ax9 = plt.subplot2grid((2,5), (1,3))
+    ax10 = plt.subplot2grid((2,5), (1,4))
+    
+    ax1.imshow(prediction[0,0,:,:,idx], cmap='gray')
+    ax1.set_title('Modality 1')
+    ax2.imshow(prediction[0,1,:,:,idx], cmap='gray')
+    ax2.set_title('Modality 2')
+    ax3.imshow(prediction[0,2,:,:,idx], cmap='gray')
+    ax3.set_title('Modality 3')
+    ax4.imshow(prediction[0,3,:,:,idx], cmap='gray')
+    ax4.set_title('Modality 4')
+    
+    ax6.imshow(mask[0,0,:,:,idx], cmap='gray')
+    ax6.set_title('Mask 1')
+    ax7.imshow(mask[0,1,:,:,idx], cmap='gray')
+    ax7.set_title('Mask 2')
+    ax8.imshow(mask[0,2,:,:,idx], cmap='gray')
+    ax8.set_title('Mask 3')
+    ax9.imshow(mask[0,3,:,:,idx], cmap='gray')
+    ax9.set_title('Mask 4')
+    
+    ax5.imshow(pred_argmax[0,:,:,idx])
+    ax5.set_title("Argmax pred")
+    ax10.imshow(mask_argmax[0,:,:,idx])
+    ax10.set_title("Argmax mask")
+    
+    for num in range(1,11):
+        exec('ax'+str(num)+'.set_yticklabels([])')
+        exec('ax'+str(num)+'.set_xticklabels([])')
+        exec('ax'+str(num)+'.set_xticks([])')
+        exec('ax'+str(num)+'.set_yticks([])')
+
+    plt.show()
+    
+    
+    
+    
